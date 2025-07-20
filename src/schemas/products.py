991bbcd78dd69fa8edffce0764d5844d79e1b988
SwDavidP <davidspuni@gmail.com>
2025-07-20 06:47:05 -0400

    feat(schemas): Define Pydantic schemas for Product and User entities

---
+++ src/schemas/products.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, ConfigDict
+
+class Product(BaseModel):
+    model_config = ConfigDict(extra='forbid')
+    name : str
+    category : str
+    price : float
+    stock : int
+    description : str
+    available : bool = True
+    
+class CreateProduct(Product):
+    pass
+    
+class UpdateProduct(BaseModel):
+    name : str | None = None
+    category : str | None = None 
+    price : float | None = None
+    stock : int | None = None
+    description : str | None = None
+    available : bool | None = None
+    
+class ReadProduct(Product):
+    model_config = ConfigDict(from_attributes=True)
+    id : int 
+    
+                                         
+                                         
+                                         
---
+++ src/schemas/users.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, ConfigDict
+
+class User(BaseModel):
+    username : str
+    available : bool = True
+    
+class CreateUser(User):
+    password : str
+     
+class UpdateUser(BaseModel):
+    username : str | None = None
+    available : bool | None = None
+    password : str | None = None
+    
+class ReadUser(User):
+    model_config = ConfigDict(from_attributes=True)
+    id : int
+    
+    
+
+    
