f062ae23b862710fefd4073945d274c11a3d4c89
SwDavidP <davidspuni@gmail.com>
2025-07-22 23:18:29 -0400

    feat(db): Configure async database session and dependency

--- src/data_base/session.py
+++ src/data_base/session.py
@@ -1,6 +1,12 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from src.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
-SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)   
+engine = create_async_engine(settings.DATABASE_URL)
+SessionLocal = async_sessionmaker(autocommit = False, autoflush = False, bind = engine)   
+
+async def get_db():
+    async with SessionLocal() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
