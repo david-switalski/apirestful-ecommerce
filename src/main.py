ab2ebcdf0ed8dfd4bb81b62e3e0966ab123c464b
SwDavidP <davidspuni@gmail.com>
2025-07-27 01:43:48 -0400

    feat(main): Initialize FastAPI app and include products and users routers

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from src.core.config import settings
 from src.data_base import base
+from src.routers.products import router as products_router
+from src.routers.users import router as users_router
 
 app = FastAPI(
     title = settings.PROJECT_NAME, 
     version = settings.PROJECT_VERSION
 )
 
+app.include_router(products_router)
+app.include_router(users_router)
+
+@app.get("/")
+async def read_root():
+    return {"message": "¡Bienvenido a mi API! Visita /docs para la documentación."}
+    
