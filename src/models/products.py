9a90fb6488f29d68b82572ded1e9224389f631a1
SwDavidP <davidspuni@gmail.com>
2025-07-27 01:42:26 -0400

    feat(database): Add timezone support to datetime columns in products and users models

--- src/models/products.py
+++ src/models/products.py
@@ -10,7 +10,7 @@ class Product(Base):
     category = Column(String(255), index=True, nullable=False)
     available = Column(Boolean, default=True)
     
-    created_at = Column(DateTime, server_default=func.now())
-    updated_at = Column(DateTime, onupdate=func.now(), nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
     
     
--- src/models/users.py
+++ src/models/users.py
@@ -7,5 +7,5 @@ class User(Base):
     hashed_password = Column(String(255), nullable=False)
     available = Column(Boolean, default=True)
     
-    created_at = Column(DateTime, server_default=func.now())
-    updated_at = Column(DateTime, onupdate=func.now(), nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
