402ee96a5192186db9f8427cb1ad72fdbb7fc853
SwDavidP <davidspuni@gmail.com>
2025-07-31 20:52:21 -0400

    feat(schemas): Update password field to SecretStr and add username length validation

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, SecretStr, field_validator
 from datetime import datetime
 from typing import Optional
 
@@ -7,12 +7,18 @@ class User(BaseModel):
     available : bool = True
     
 class CreateUser(User):
-    password : str
+    password : SecretStr
+    
+    @field_validator('username')
+    def username_length(cls, v):
+        if len(v) < 3 or len(v) > 50:
+            raise ValueError("Username must be between 3 and 50 characters")
+        return v
      
 class UpdateUser(BaseModel):
     username : str | None = None
     available : bool | None = None
-    password : str | None = None
+    password : SecretStr | None = None
     
 class ReadUser(User):
     model_config = ConfigDict(from_attributes=True, arbitrary_types_allowed=True)
