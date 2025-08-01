55a42024aae702d8f034e0d5e9bc7bd2cae27938
SwDavidP <davidspuni@gmail.com>
2025-07-31 21:28:43 -0400

    feat(models): Enhance user model by increasing hashed_password length and adding indexes for created_at and updated_at

--- src/models/users.py
+++ src/models/users.py
@@ -5,8 +5,8 @@ from datetime import datetime
 class User(Base):
     id = Column(BigInteger,primary_key=True, index=True, autoincrement=True)
     username = Column(String(255), unique=True, index=True, nullable=False)
-    hashed_password = Column(String(255), nullable=False)
+    hashed_password = Column(String(512), nullable=False)
     available = Column(Boolean, default=True)
     
-    created_at = Column(DateTime(timezone=True), server_default=func.now(), default=datetime.utcnow)
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), default=datetime.utcnow)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), default=datetime.utcnow, index=True)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), default=datetime.utcnow, index=True)
