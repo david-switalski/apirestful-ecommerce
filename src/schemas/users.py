fd4356632d12390045bb322a31f14eda982283c4
SwDavidP <davidspuni@gmail.com>
2025-08-01 05:08:44 -0400

    feat(schemas): Add password validation to CreateUser schema

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, ConfigDict, SecretStr, field_validator
 from datetime import datetime
 from typing import Optional
+import regex as re
 
 class User(BaseModel):
     username : str
@@ -13,6 +14,14 @@ class CreateUser(User):
     def username_length(cls, v):
         if len(v) < 3 or len(v) > 50:
             raise ValueError("Username must be between 3 and 50 characters")
+        return v
+    
+    @field_validator('password')
+    def password_validation(cls, v):
+        regex = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,50}$"
+        
+        if not re.search(regex, v.get_secret_value()):
+            raise ValueError("The password must be between 8 and 50 characters and must contain at least one uppercase letter, one lowercase letter, one integer and one character.")
         return v
      
 class UpdateUser(BaseModel):
