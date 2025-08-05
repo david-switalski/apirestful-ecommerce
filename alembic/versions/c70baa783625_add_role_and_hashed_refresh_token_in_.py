6d6aaadb391c6ace258f4e18432fd08dc7436a7e
SwDavidP <davidspuni@gmail.com>
2025-08-05 06:23:07 -0400

    feat(models): Add role and hashed_refresh_token fields to User model

---
+++ alembic/versions/c70baa783625_add_role_and_hashed_refresh_token_in_.py
@@ -0,0 +1,40 @@
+"""Add role and hashed_refresh_token in User model
+
+Revision ID: c70baa783625
+Revises: 04ae7ff32cdb
+Create Date: 2025-08-05 10:17:23.439259
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'c70baa783625' # pragma: allowlist secret
+down_revision: Union[str, Sequence[str], None] = '04ae7ff32cdb'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    
+    op.execute("CREATE TYPE user_role_enum AS ENUM ('admin', 'user');")
+
+    
+    op.add_column('user', sa.Column('role', sa.Enum('admin', 'user', name='user_role_enum'), nullable=False))
+    op.add_column('user', sa.Column('hashed_refresh_token', sa.String(length=512), nullable=True))
+    # ### end Alembic commands ###
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+   
+    op.drop_column('user', 'hashed_refresh_token')
+    op.drop_column('user', 'role')
+
+   
+    op.execute("DROP TYPE user_role_enum;")
+    # ### end Alembic commands ###
--- src/models/users.py
+++ src/models/users.py
@@ -1,11 +1,18 @@
-from sqlalchemy import Column, String, BigInteger, Boolean, DateTime, func
+from sqlalchemy import Column, String, BigInteger, Boolean, DateTime, func, Enum
 from src.data_base.base_class import Base
 from datetime import datetime
+import enum
 
+class UserRole(str, enum.Enum):
+    admin = "admin"
+    user = "user"
+    
 class User(Base):
     id = Column(BigInteger,primary_key=True, index=True, autoincrement=True)
     username = Column(String(255), unique=True, index=True, nullable=False)
+    role = Column(Enum(UserRole, name='user_role_enum'), nullable=False, default=UserRole.user)
     hashed_password = Column(String(512), nullable=False)
+    hashed_refresh_token = Column(String(512), nullable=True)
     available = Column(Boolean, default=True)
     
     created_at = Column(DateTime(timezone=True), server_default=func.now(), default=datetime.utcnow, index=True)
