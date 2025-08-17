88e83df0ccd2dfb5400b0e322a288e905d0e1544
SwDavidP <davidspuni@gmail.com>
2025-08-17 07:03:22 -0400

    fix: Update database URL configuration in env.py and add pytest.ini for asyncio support

--- alembic/env.py
+++ alembic/env.py
@@ -5,8 +5,7 @@ from alembic import context
 import os 
 from dotenv import load_dotenv
 
-
-from asyncio import run as run_async
+import asyncio
 from sqlalchemy.ext.asyncio import AsyncEngine
 
 
@@ -20,13 +19,17 @@ load_dotenv()
 
 config = context.config
 
-alembic_database_url = os.environ.get('DATABASE_URL') 
-
-if not alembic_database_url:
-    alembic_database_url = os.environ.get('DATABASE_URL')
-
-
-config.set_main_option('sqlalchemy.url', alembic_database_url)
+if config.get_main_option("sqlalchemy.url") is None:
+    alembic_database_url = os.environ.get('DATABASE_URL_ALEMBIC')
+    if not alembic_database_url:
+        alembic_database_url = os.environ.get('DATABASE_URL')
+    
+    if alembic_database_url is None:
+        raise ValueError(
+            "The database URL is not configured."
+        )
+        
+    config.set_main_option('sqlalchemy.url', alembic_database_url)
 
 
 if config.config_file_name is not None:
@@ -80,8 +83,12 @@ async def run_async_migrations():
 
 
 def run_migrations_online() -> None:
-   
-    run_async(run_async_migrations())
+    asyncio.run(run_async_migrations())
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
 
 
 if context.is_offline_mode():
